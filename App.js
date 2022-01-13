d3aaaac03f74eaf11f4a4b166a2f63c20c6c86f9
mdniyaz224 <68887332+mdniyaz224@users.noreply.github.com>
2022-01-13 20:16:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+   
+    <h1 className='animate__animated animate__flip'>An animated element</h1>
+    <h1 class="animate__animated  animate__lightSpeedInRight">An animated element</h1>
+    <h1 className='animate__animated animate__lightSpeedInLeft'>An animated element</h1>
+    <h1 class="animate__animated  animate__backOutRight">An animated element</h1>
+    <h1 className='animate__animated animate__flip'>An animated element</h1>
+    <h1 class="animate__animated  animate__backOutRight">An animated element</h1>
+    <h1 className='animate__animated animate__flip'>An animated element</h1>
+    <h1 class="animate__animated  animate__backOutRight">An animated element</h1>
+    <h1 className='animate__animated animate__flip'>An animated element</h1>
+    <h1 class="animate__animated  animate__backOutRight">An animated element</h1>
+    </>
+  
+  );
+}
+
+export default App;
